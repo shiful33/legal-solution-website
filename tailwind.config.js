6b71ba0ae2d654cc7009b0ea03fa444fef4476e4
shiful33 <shwapon.joti@gmail.com>
2025-07-20 06:02:52 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+    module.exports = {
+      content: [
+        "./**/*.{html,js}",
+        // Add paths to your template files here
+      ],
+      theme: {
+        extend: {
+          // Customize your theme here
+        },
+      },
+      plugins: [],
+    }
